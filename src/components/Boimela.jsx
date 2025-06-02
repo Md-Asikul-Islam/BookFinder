6d693f341bcc9e05589073e97b29a338c1242945
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-06-02 15:11:21 +0600

    all component are ready

--- src/components/Boimela.jsx
+++ src/components/Boimela.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { Books } from "../data";
+import Header from "./Header";
+import SearchBar from "./SearchBar";
+import BookList from "./BookList";
 
 function Boimela() {
   return (
-    
+    <>
+      <Header />
+      <SearchBar />
+      <BookList />
+    </>
   );
 }
 
---
+++ src/components/BookRow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BookRow() {
+  return (
+    <div>BookRow</div>
+  )
+}
+
+export default BookRow
