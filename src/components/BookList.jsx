6cb06590fe1fdf5e9b05ec57d335ca5e61ab8504
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-06-02 17:35:57 +0600

    adding some styles for search bar

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,12 +1,14 @@
-import React from "react";
 import BookRow from "./BookRow";
 
-function BookList({searchTerm, books, onFeatureBook}) {
-  return (
-    <>
-      <BookRow/>
-    </>
-  );
-}
+function BookList({ searchTerm, books, onFeatureBook }) {
+    const rows = [];
+    books.forEach((book) => {
+        if (book.title.toLowerCase().indexOf(searchTerm.toLowerCase()) === -1) {
+            return;
+        }
+        rows.push(<BookRow key={book.id} book={book} onFeatureBook={onFeatureBook} />);
+    });
 
+    return <div className="space-y-4">{rows}</div>;
+}
 export default BookList;
