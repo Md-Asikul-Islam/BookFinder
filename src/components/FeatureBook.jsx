c02442148b23a8092628b21300c44f97c15303e9
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-06-04 00:39:43 +0600

    updata design

--- src/components/FeatureBook.jsx
+++ src/components/FeatureBook.jsx
@@ -3,7 +3,7 @@ import { Star } from "lucide-react";
 function FeatureBook({ book, onFeatureBook }) {
   return (
     <button className="cursor-pointer" onClick={() => onFeatureBook(book.id)}>
-      <Star color={book.featured ? "Green" : "Black"} />
+      <Star color={book.featured ? "Yellow" : "Black"} />
     </button>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function SearchBar({ searchTerm, onSearchTerm }) {
   return (
-    <div className="w-full px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12">
+    <div className="w-full">
       <form className="mx-auto max-w-4xl">
         <input
           type="text"
           placeholder=" Search for a book..."
           value={searchTerm}
           onChange={(e) => onSearchTerm(e.target.value)}
-          className="w-full px-5 py-6 mb-3 text-base text-gray-800 placeholder-gray-500
+          className="w-full  px-5 py-6 mb-3 text-base text-gray-800 placeholder-gray-500
                      border border-gray-300 rounded-2xl shadow-sm 
                      focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500
                      transition duration-300 ease-in-out"
